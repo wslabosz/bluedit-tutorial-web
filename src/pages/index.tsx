4944a4429e8877c5a70ff52474458019283d909b
Wojciech Slabosz <230000@edu.p.lodz.pl>
2021-08-09 19:51:59 +0200

    added server side rendering for homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,20 @@
 import { withUrqlClient } from 'next-urql'
 import { NavBar } from '../components/NavBar'
+import { usePostsQuery } from '../generated/graphql'
 import { createUrqlClient } from '../utils/createUrqlClient'
 
-const Index = () => (
-   <>
-      <NavBar />
-   </>
-)
+const Index = () => {
+   const [{ data }] = usePostsQuery()
+   return (
+      <>
+         <NavBar />
+         {!data ? (
+            <div>loading...</div>
+         ) : (
+            data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+         )}
+      </>
+   )
+}
 
-export default withUrqlClient(createUrqlClient)(Index)
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index)
