4777dd8fc598e2017e93f77f7bb8cd1bfd8d6e73
Wojciech Slabosz <230000@edu.p.lodz.pl>
2021-08-13 20:56:44 +0200

    added routing to create-post

--- src/pages/create-post.tsx
+++ src/pages/create-post.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Box, Button } from '@chakra-ui/react'
 import { Formik, Form } from 'formik'
 import { InputField } from '../components/InputField'
-import { useCreatePostMutation } from '../generated/graphql'
+import { useCreatePostMutation, useMeQuery } from '../generated/graphql'
 import { useRouter } from 'next/router'
 import { createUrqlClient } from '../utils/createUrqlClient'
 import { withUrqlClient } from 'next-urql'
 import { Layout } from '../components/Layout'
 
 const CreatePost: React.FC<{}> = ({}) => {
+   const [{ data, fetching }] = useMeQuery()
    const router = useRouter()
+   useEffect(() => {
+      if (!fetching && !data?.me) {
+         router.replace('/login')
+      }
+   }, [fetching, data, router])
    const [, createPost] = useCreatePostMutation()
    return (
       <Layout variant="small">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,24 @@
-import { NavBar } from '../components/NavBar'
 import { usePostsQuery } from '../generated/graphql'
 import { withUrqlClient } from 'next-urql'
 import { createUrqlClient } from '../utils/createUrqlClient'
+import { Layout } from '../components/Layout'
+import { Link } from '@chakra-ui/react'
+import NextLink from 'next/link'
 
 const Index = () => {
    const [{ data }] = usePostsQuery()
    return (
-      <>
-         <NavBar />
+      <Layout>
+         <NextLink href="/create-post">
+            <Link>create post</Link>
+         </NextLink>
+         <br />
          {!data ? (
             <div>loading...</div>
          ) : (
             data.posts.map((p) => <div key={p.id}>{p.title}</div>)
          )}
-      </>
+      </Layout>
    )
 }
 
